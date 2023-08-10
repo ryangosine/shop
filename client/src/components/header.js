c2d2b6058566b566ce9fe4c6c1e66be192199e3e
Ryan Gosine <ryanganeshgosineyarn test exercise-6git config --global user.email ryanganeshgosine>
2023-08-09 21:12:20 -0400

    updated header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,15 +1,45 @@
 import React from "react";
-import { styled } from "styled-components";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Header = () => {
   return (
     <HeaderWrapper>
       <HeaderTitle>Shop</HeaderTitle>
+      <Nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/register">Register</NavLink>
+        {/* Add more navigation links */}
+      </Nav>
     </HeaderWrapper>
   );
 };
 
-const HeaderWrapper = styled.div``;
-const HeaderTitle = styled.h1``;
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #333;
+  color: white;
+`;
+
+const HeaderTitle = styled.h1`
+  margin: 0;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  gap: 1rem;
+`;
+
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 export default Header;
